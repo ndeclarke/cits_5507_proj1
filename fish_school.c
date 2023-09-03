dc50773e6f7930dd3d0034c684e503dde0471f7c
Nicholas Clarke <ndeclarke@gmail.com>
2023-09-03 17:12:51 +0800

    fixed pointers in calcs

--- fish_school.c
+++ fish_school.c
@@ -9,12 +9,16 @@ static int max_steps = 10; // how many iterations of swimming to simulate
 void main()
 {
 	float max_delta;
+	float bary_numer; // technically should have x and y for barycentre
+	// but project requirements do not give that equation
+	float bary_denom;
 	struct fish{
 		float x;
 		float y;
 		float w;
 		float move_x;
 		float move_y;
+		float euc_dist;
 		float delta_f;
 	};
 	
@@ -33,23 +37,31 @@ void main()
 		for(i = 0; i<school; i = i +1){
 			(fishes + i) -> move_x = ((float)rand()/(float)(RAND_MAX)-0.5)*move_speed;
 			(fishes + i) -> move_y = ((float)rand()/(float)(RAND_MAX)-0.5)*move_speed;
-			(fishes + i) -> delta_f = sqrt((x + move_x)**2 + (y + move_y)**2) - sqrt(x**2 + y**2);
-			(fishes + i) -> x = x + move_x;
-			(fishes + i) -> y = y + move_y;
+			(fishes + i) -> x = (fishes + i) -> x + (fishes + i) -> move_x;
+			(fishes + i) -> y = (fishes + i) -> y + (fishes + i) -> move_y;
+			(fishes + i) -> delta_f = sqrt((fishes + i) -> x**2 + (fishes + i) -> move_y**2) - (fishes + i) -> euc_dist;
+			(fishes + i) -> euc_dist = sqrt((fishes + i) -> x**2 + (fishes + i) -> move_y**2);
 		}
+
 		// find maximum change to objective function
 		max_delta = -0.2 // minimum possible is 0.1*sqrt(2)
 		for(i = 0; i<school; i = i +1){
-			if((fishes + i).delta_f > max_delta){max_delta = (fishes + i).delta_f;
+			if((fishes + i) -> delta_f > max_delta){max_delta = (fishes + i) -> delta_f;}
 		}
+
 		//change weights
 		for(i = 0; i<school; i = i +1){
-			(fishes + i) -> w = w + delta_f/max_delta;
+			(fishes + i) -> w = w + (fishes + i) -> delta_f/max_delta;
 		}
+
 		// calculate barycentre
+		bary_numer = 0;
+		bary_denom = 0;
 		for(i = 0; i<school; i = i +1){
-			//sum
+			bary_numer = bary_numer + (fishes + i) -> euc_dist * (fishes + i) -> w;
+			bary_denom = bary_denom + (fishes + i) -> euc_dist;
 		}
+		printf("barycentre =%f\n", bary_numer/bary_denom);
 	}
 }
 
